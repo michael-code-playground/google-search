6d933bd9c02a80ab5e22c7edcc828da4e8870391
unknown <michaljaroch@hotmail.com>
2023-11-03 22:27:04 +0100

    Tests

--- search.py
+++ search.py
@@ -1,4 +1,12 @@
 from googlesearch import search
-
-for result in search("on-call interpreting", advanced=True):
+from email_scraper import *
+for result in search("facial hair products", advanced=True, num_results=5):
+    
+    if "amazon" in result.url:
+        continue
+    emails = scrape_mail(result.url)
+    
+    
     print(f'Title: {result.title}, Link: {result.url}')
+    print()
+    print(f'Scraped mails: {emails}')
